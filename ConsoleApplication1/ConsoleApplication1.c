85f1570354e58bf7d2e813a6244e16c8ae9e4473
dmarenin <39666107+dmarenin@users.noreply.github.com>
2020-04-09 01:12:50 +0500

    init

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,32 @@
+﻿/* Example of embedding Python in another program */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include "Python.h"
+
+main(int argc, char** argv)
+{
+
+	Py_SetProgramName(argv[0]);
+
+	Py_Initialize();
+
+	PySys_SetArgvEx(argc, argv, 0);
+
+	while (1) {
+
+		char str[80];
+		gets(str);
+		printf("=>", strlen(str));
+		PyRun_SimpleString(str);
+	}
+
+	
+	Py_Finalize();
+
+
+	Py_Exit(0);
+
+}
+
